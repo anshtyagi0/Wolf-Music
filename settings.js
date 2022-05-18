af3af922a1e53a1655080ca512f2c5ec146ceb6d
AkAbhijit <abhijitkirtunia011@gmail.com>
2022-05-18 20:22:38 +0530

    main

---
+++ settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  token: "OTcyMTk2MTgyMzg0MzM3MDM2.GSIyOR.7ofpeFS_XrZd0WAWP_3GTMjK56CMDw7pW_Byrc",
+  mongo: "mongodb+srv://Apera:6291@cluster0.7qkb4.mongodb.net/aperatest",
+  prefix: ".",
+  audionodes: [
+    {
+      identifier: "local",
+      host: "50.19.31.157",
+      port: 80,
+      password: "coders",
+    },
+  ],
+
+  embed_color: "#2f3136",
+  icon: "https://cdn.discordapp.com/attachments/967885404462252113/973128568106279011/Apera.jpg",
+};
